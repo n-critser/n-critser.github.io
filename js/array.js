9cad4a6429cac9f3e8c270fd0408cdb4e893b147
n-critser <critser101@gmail.com>
2014-01-18 22:51:20 -0500

    working on js

--- js/array.js
+++ js/array.js
@@ -1,9 +1,14 @@
-function go(){
+function go(element){
     var array = new Array();
     array[0] = "Red";
     array[1] = "Blue";
     array[3] = "Green";
+
+    var ul = document.createElement("ul");
     for (var i=0; i < array.length; i++){
-        document.write("<li>" + array[i] + "<br />");
+	var li = document.createElement("li");
+	li.innerHtml = array[i];
+	ul.appendChild(li);
     }
+    body.insertAfter(ul, element);
 }
