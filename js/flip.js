3b51836e0284be354c74e8c3d2c079cb736ddf34
n-critser <critser101@gmail.com>
2014-02-13 17:30:59 -0500

    js

--- js/flip.js
+++ js/flip.js
@@ -1,6 +1,5 @@
  <!-- hide script from old browsers
 document.write('<FONT COLOR=green>This is flip.js</FONT>')
-onMouseOver="document.pis1.src='./assets/bullet.png'"
-onMouseOver="document.pic1.src='./assets/Nebula_128.png'"
+
 // end hiding script from old browsers
 -->
