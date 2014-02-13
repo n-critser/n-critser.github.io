8816c6ac16d71ef74142fad211946cd292941cbf
n-critser <critser101@gmail.com>
2014-02-13 17:55:42 -0500

    js

--- js/flip.js
+++ js/flip.js
@@ -3,6 +3,22 @@ document.write('<FONT COLOR=green>This is flip.js</FONT>')
 
 var age= 25;
 var introText = "A long, long time ago...";
-document.write(introText + "I was" + age);
+document.write(introText + "I was  " + age);
+
+var todos = new Array();
+todos[0] = "TODO:";
+todos[1] = "CHECKOUT:";
+todos[2] = "BUG:";
+
+var str = "Contact me at nick@nickcritser.com"
+var frags = new Array();
+frags = b.split(' ');
+
+if ((frags != 0) || (str.length() < 5)){
+    document.write("frags[2]");
+}
+
+document.write( todos[0]);
+
 // end hiding script from old browsers
 -->
