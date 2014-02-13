de397af24d2064143fa8e2db2f9216c890a5c25a
n-critser <critser101@gmail.com>
2014-02-13 17:45:50 -0500

    js

--- js/flip.js
+++ js/flip.js
@@ -1,5 +1,8 @@
  <!-- hide script from old browsers
 document.write('<FONT COLOR=green>This is flip.js</FONT>')
 
+var age= 25;
+var introText = "A long, long time ago...";
+document.write(introText + "I was" + age);
 // end hiding script from old browsers
 -->
